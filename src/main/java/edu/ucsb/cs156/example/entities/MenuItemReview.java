6a074ad22e47994339790388e2f628c0b7a6258c
Yvonneliu0201 <yvonneliu@ucsb.edu>
2022-04-26 22:38:02 -0700

    yl - finished database for menuitemreview

--- src/main/java/edu/ucsb/cs156/example/entities/MenuItemReview.java
+++ src/main/java/edu/ucsb/cs156/example/entities/MenuItemReview.java
@@ -1,4 +1,4 @@
-package main.java.edu.ucsb.cs156.example.entities;
+package edu.ucsb.cs156.example.entities;
 
 import java.time.LocalDateTime;
 
--- src/main/java/edu/ucsb/cs156/example/repositories/MenuItemReviewRepository.java
+++ src/main/java/edu/ucsb/cs156/example/repositories/MenuItemReviewRepository.java
@@ -1,6 +1,6 @@
-package main.java.edu.ucsb.cs156.example.repositories;
+package edu.ucsb.cs156.example.repositories;
 
-import main.java.edu.ucsb.cs156.example.entities.MenuItemReview;
+import edu.ucsb.cs156.example.entities.MenuItemReview;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
