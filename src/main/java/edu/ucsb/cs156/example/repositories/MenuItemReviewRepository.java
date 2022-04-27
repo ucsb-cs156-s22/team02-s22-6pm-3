05f634a06cb76b9cfba79f8ec580cb3fc635bf08
Yvonneliu0201 <yvonneliu@ucsb.edu>
2022-04-26 22:31:06 -0700

    added menuitemreview database

---
+++ src/main/java/edu/ucsb/cs156/example/repositories/MenuItemReviewRepository.java
@@ -0,0 +1,12 @@
+package main.java.edu.ucsb.cs156.example.repositories;
+
+import main.java.edu.ucsb.cs156.example.entities.MenuItemReview;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface MenuItemReviewRepository extends CrudRepository<MenuItemReview, Long> {
+
+}
