51e40541bb1ef1cbcf66f4a140bd7d80afbc07ce
Ayush Shah <ayushshah@ucsb.edu>
2022-05-03 09:25:01 -0700

    added tests for logged in/out users for all and post

---
+++ src/test/java/edu/ucsb/cs156/example/controllers/UCSBDiningCommonsMenuItemControllerTests.java
@@ -0,0 +1,78 @@
+// package edu.ucsb.cs156.example.controllers;
+
+// import edu.ucsb.cs156.example.repositories.UserRepository;
+// import edu.ucsb.cs156.example.testconfig.TestConfig;
+// import edu.ucsb.cs156.example.ControllerTestCase;
+// import edu.ucsb.cs156.example.entities.UCSBDiningCommonsMenuItem;
+// import edu.ucsb.cs156.example.repositories.UCSBDiningCommonsMenuItemRepository;
+
+// import java.util.ArrayList;
+// import java.util.Arrays;
+// import java.util.Map;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+// import org.springframework.boot.test.mock.mockito.MockBean;
+// import org.springframework.context.annotation.Import;
+// import org.springframework.http.MediaType;
+// import org.springframework.security.test.context.support.WithMockUser;
+// import org.springframework.test.web.servlet.MvcResult;
+
+// import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+// import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+// import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+
+// import java.util.Optional;
+
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+// import static org.mockito.ArgumentMatchers.any;
+// import static org.mockito.ArgumentMatchers.eq;
+// import static org.mockito.Mockito.times;
+// import static org.mockito.Mockito.verify;
+// import static org.mockito.Mockito.when;
+
+// @WebMvcTest(controllers = UCSBDiningCommonsController.class)
+// @Import(TestConfig.class)
+// public class UCSBDiningCommonsMenuItemControllerTests extends ControllerTestCase {
+
+//         @MockBean
+//         UCSBDiningCommonsMenuItemRepository ucsbDiningCommonsMenuItemRepository;
+
+//         @MockBean
+//         UserRepository userRepository;
+//         @Test
+//         public void logged_out_users_cannot_get_all() throws Exception {
+//                 mockMvc.perform(get("/api/UCSBDiningCommonsMenuItem/all"))
+//                                 .andExpect(status().is(403)); // logged out users can't get all
+//         // Authorization tests for /api/ucsbdiningcommonsmenuitem/admin/all
+//         @WithMockUser(roles = { "USER" })
+//         @Test
+//         public void logged_in_users_can_get_all() throws Exception {
+//                 mockMvc.perform(get("/api/UCSBDiningCommonsMenuItem/all"))
+//                                 .andExpect(status().is(200)); // logged
+//         }
+
+//         @Test
+//         public void logged_out_users_cannot_get_by_id() throws Exception {
+//                 mockMvc.perform(get("/api/UCSBDiningCommonsMenuItem?id=1"))
+//                                 .andExpect(status().is(403)); // logged out users can't get by id
+//         }
+
+//         // Authorization tests for /api/UCSBDiningCommonsMenuItem/post
+//         // (Perhaps should also have these for put and delete)
+
+//         @Test
+//         public void logged_out_users_cannot_post() throws Exception {
+//                 mockMvc.perform(post("/api/UCSBDiningCommonsMenuItem/post"))
+//                                 .andExpect(status().is(403));
+//         }
+
+//         @WithMockUser(roles = { "USER" })
+//         @Test
+//         public void logged_in_regular_users_cannot_post() throws Exception {
+//                 mockMvc.perform(post("/api/UCSBDiningCommonsMenuItem/post"))
+//                                 .andExpect(status().is(403)); // only admins can post
+//         }
+
+// }
+
+
